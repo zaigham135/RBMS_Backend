360240f54890a292108ecf7f6727d8fd712cf97c
zaigham <zaigham135@gmail.com>
2026-04-07 03:21:00 +0530

    Fourth  commit with response update and logging feature.

---
+++ backend/backend/src/main/java/com/example/backend/dto/response/UserResponse.java
@@ -0,0 +1,21 @@
+package com.example.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String role;
+    private String status;
+    private String profilePhoto;
+    private LocalDateTime createdAt;
+}
